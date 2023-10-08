1fe764845bcb86ba2934c6cda5ce58beae5d437c
natdogg <natdogg@outlook.com>
2023-10-08 21:38:37 +1300

    feat:adding terraform checkov testing

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -29,9 +31,30 @@ data "azurerm_resource_group" "rg" {
 
 # Test resource
 resource "azurerm_storage_account" "test_sa" {
-  account_replication_type = "LRS"
+  account_replication_type = "GRS"
   account_tier             = "Standard"
   location                 = "australiaeast"
   name                     = "sanstestaccount01"
   resource_group_name      = data.azurerm_resource_group.rg.name
+
+  # Security additions
+  min_tls_version = "TLS1_2"
+  queue_properties {
+    logging {
+      delete  = true
+      read    = true
+      write   = true
+      version = 1.0
+    }
+  }
+  public_network_access_enabled = false
+
+}
+
+resource "azurerm_log_analytics_workspace" "test_la" {
+  name                = "la-ns-testws01"
+  location            = "australiaeast"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 7
 }
